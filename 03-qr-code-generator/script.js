d0b246205a9ba666822670bb90b82cae1a5d0c94
Mohamud Dahir <siyaasi54@gmail.com>
2025-04-07 21:50:27 +0300

    Initiated script and added DOM variables for QR input, buttons, and preview section

---
+++ 03-qr-code-generator/script.js
@@ -0,0 +1,6 @@
+const qrText = Document.getElementById("qr-text");
+const generateBtn = document.getElementById("generateBtn");
+const clearBtn = document.getElementById("clearBtn");
+const qrBox = document.getElementById("qrBox");
+const qrCode =  document.getElementById("qrCode");
+const  status = document.getElementById("status")
