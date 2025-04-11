cb6c5f1eeff4d88a5d497e016ce4ab25ec817f80
Mohamud Dahir <siyaasi54@gmail.com>
2025-04-12 00:04:11 +0300

    Initialize DOM Elements

---
+++ 04-digital-clock/script.js
@@ -0,0 +1,11 @@
+// DOM Elements
+const hourEl = document.querySelector('.hour');
+const minutesEl = document.querySelector('.minutes');
+const secondsEl = document.querySelector('.seconds');
+const periodEl = document.querySelector('.period');
+const monthsEl = document.querySelector('.month-name');
+const daysEl = document.querySelector('.day-name');
+const dayNumbersEl = document.querySelector('.day-number');
+const yearsEl = document.querySelector('.year');
+const themeToggle = document.getElementById('themeToggle');
+const formatRadios = document.querySelectorAll('input[name="timeFormat"]');
