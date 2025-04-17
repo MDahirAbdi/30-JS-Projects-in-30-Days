b63b779f404fd7253218b82443d8a384def054e8
Mohamud Dahir <siyaasi54@gmail.com>
2025-04-17 21:06:50 +0300

    initiate js

---
+++ 06-countdown-timer/script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => { 
+let timer;
+let totalSeconds = 0;
+let isRunning = false;
+let isPaused = false;
+
+const inputHours = document.getElementById('inputHours');
+const inputMinutes = document.getElementById('inputMinutes');
+const inputSeconds = document.getElementById('inputSeconds');
+const timerDisplay = document.getElementById('timer');
+const startBtn = document.getElementById('startBtn');
+const resetBtn = document.getElementById('resetBtn');
+const lapBtn = document.getElementById('lapBtn');
+const lapsContainer = document.getElementById('laps');
+const alarmSound = document.getElementById('alarmSound');
+
+
+});
